a767bfbf3a8d3d90c777ab66ec2d862528f597d0
Dan Bennett <dghbennett47@gmail.com>
2021-08-01 22:46:29 +0100

    chore: add basic setup

--- reddit-server/src/index.ts
+++ reddit-server/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello world");
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+
+const main = async () => {
+	const orm = await MikroORM.init({
+		dbName: 'lireddit',
+		type: 'postgresql',
+		debug: !__prod__,
+	});
+};
+
+main();
