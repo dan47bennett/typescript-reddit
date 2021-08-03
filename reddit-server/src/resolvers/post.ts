9255cbaa46f32fdb8ee58cf29864285630b54743
Dan Bennett <dghbennett47@gmail.com>
2021-08-04 00:18:44 +0100

    feat: add PostResolver and connect to orm

---
+++ reddit-server/src/resolvers/post.ts
@@ -0,0 +1,11 @@
+import { MyContext } from 'src/types';
+import { Ctx, Query, Resolver } from 'type-graphql';
+import { Post } from '../entities/Post';
+
+@Resolver()
+export class PostResolver {
+	@Query(() => [Post])
+	posts(@Ctx() ctx: MyContext): Promise<Post[]> {
+		return ctx.em.find(Post, {});
+	}
+}
