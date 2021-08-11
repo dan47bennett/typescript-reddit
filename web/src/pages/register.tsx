cdd8e881628ff94eccfc9ccb2b897da50346a79d
Dan Bennett <dghbennett47@gmail.com>
2021-08-11 01:56:37 +0100

    feat: use urql to add register button

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,55 +1,74 @@
-import React from "react";
-import { Formik, Form } from "formik";
+import React from 'react';
+import { Formik, Form } from 'formik';
 import {
-  FormControl,
-  FormLabel,
-  Input,
-  FormErrorMessage,
-  Box,
-  Button,
-} from "@chakra-ui/core";
-import { Wrapper } from "../components/Wrapper";
-import { InputField } from "../components/InputField";
+	FormControl,
+	FormLabel,
+	Input,
+	FormErrorMessage,
+	Box,
+	Button,
+} from '@chakra-ui/core';
+import { Wrapper } from '../components/Wrapper';
+import { InputField } from '../components/InputField';
+import { useMutation } from 'urql';
 
 interface registerProps {}
 
+const REGISTER_MUT = `
+mutation Register($username: String!, $password: String!) {
+  register(options: { username: $username, password: $password }) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+`;
+
 const Register: React.FC<registerProps> = ({}) => {
-  return (
-    <Wrapper variant="small">
-      <Formik
-        initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField
-              name="username"
-              placeholder="username"
-              label="Username"
-            />
-            <Box mt={4}>
-              <InputField
-                name="password"
-                placeholder="password"
-                label="Password"
-                type="password"
-              />
-            </Box>
-            <Button
-              mt={4}
-              type="submit"
-              isLoading={isSubmitting}
-              variantColor="teal"
-            >
-              register
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </Wrapper>
-  );
+	const [, register] = useMutation(REGISTER_MUT);
+
+	return (
+		<Wrapper variant="small">
+			<Formik
+				initialValues={{ username: '', password: '' }}
+				onSubmit={(values) => {
+					console.log(values);
+					return register(values);
+				}}
+			>
+				{({ isSubmitting }) => (
+					<Form>
+						<InputField
+							name="username"
+							placeholder="username"
+							label="Username"
+						/>
+						<Box mt={4}>
+							<InputField
+								name="password"
+								placeholder="password"
+								label="Password"
+								type="password"
+							/>
+						</Box>
+						<Button
+							mt={4}
+							type="submit"
+							isLoading={isSubmitting}
+							variantColor="teal"
+						>
+							register
+						</Button>
+					</Form>
+				)}
+			</Formik>
+		</Wrapper>
+	);
 };
 
 export default Register;
