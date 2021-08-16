3acb7d21ddb121eb760df78e1c041630a8b2c065
Dan Bennett <dghbennett47@gmail.com>
2021-08-16 14:33:39 +0100

    feat: update cache when login completed

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,9 +1,67 @@
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
-import { Provider, createClient } from 'urql';
+import { Provider, createClient, dedupExchange, fetchExchange } from 'urql';
+import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
 import theme from '../theme';
+import {
+	LoginMutation,
+	MeDocument,
+	MeQuery,
+	RegisterMutation,
+} from '../generated/graphql';
+
+function betterUpdateQuery<Result, Query>(
+	cache: Cache,
+	qi: QueryInput,
+	result: any,
+	fn: (r: Result, q: Query) => Query
+) {
+	return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
 	url: 'http://localhost:4000/graphql',
+	exchanges: [
+		dedupExchange,
+		cacheExchange({
+			updates: {
+				Mutation: {
+					login: (result: LoginMutation, args, cache, info) => {
+						betterUpdateQuery<LoginMutation, MeQuery>(
+							cache,
+							{ query: MeDocument },
+							result,
+							(result, query) => {
+								if (result.login.errors) {
+									return query;
+								} else {
+									return {
+										me: result.login.user,
+									};
+								}
+							}
+						);
+					},
+					register: (result: RegisterMutation, args, cache, info) => {
+						betterUpdateQuery<RegisterMutation, MeQuery>(
+							cache,
+							{ query: MeDocument },
+							result,
+							(result, query) => {
+								if (result.register.errors) {
+									return query;
+								} else {
+									return {
+										me: result.register.user,
+									};
+								}
+							}
+						);
+					},
+				},
+			},
+		}),
+		fetchExchange,
+	],
 	fetchOptions: {
 		credentials: 'include',
 	},
