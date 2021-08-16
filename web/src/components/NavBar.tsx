42d522078a055f47d8c8788c04086be3a1419673
Dan Bennett <dghbennett47@gmail.com>
2021-08-16 14:01:59 +0100

    feat: state changes by login

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,19 +1,41 @@
 import React from 'react';
-import { Box, Flex, Link } from '@chakra-ui/core';
+import { Box, Button, Flex, Link } from '@chakra-ui/core';
 import NextLink from 'next/link';
+import { useMeQuery } from '../generated/graphql';
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-	return (
-		<Flex bg="beige" p={3}>
-			<Box ml={'auto'} color="black">
+	const [{ data, fetching }] = useMeQuery();
+	let body = null;
+
+	if (fetching) {
+		// loading
+	} else if (!data?.me) {
+		// logged in
+		body = (
+			<>
 				<NextLink href="/login">
 					<Link mr={3}>Login</Link>
 				</NextLink>
 				<NextLink href="/register">
 					<Link mr={3}>Register</Link>
 				</NextLink>
+			</>
+		);
+	} else {
+		body = (
+			<>
+				<Box mr={3}>{data.me.username}</Box>
+				<Button variant="link">Logout</Button>
+			</>
+		);
+	}
+
+	return (
+		<Flex bg="beige" p={3}>
+			<Box ml={'auto'} display="flex" flexDirection="row">
+				{body}
 			</Box>
 		</Flex>
 	);
