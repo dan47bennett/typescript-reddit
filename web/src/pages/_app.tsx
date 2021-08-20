6735eecef25495585480afbc3038143ec9815c26
Dan Bennett <dghbennett47@gmail.com>
2021-08-20 11:00:41 +0100

    feat: refactor urql client and add SSR

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,94 +1,12 @@
-import { ThemeProvider, CSSReset } from '@chakra-ui/core';
-import { Provider, createClient, dedupExchange, fetchExchange } from 'urql';
-import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
+import { CSSReset, ThemeProvider } from '@chakra-ui/core';
 import theme from '../theme';
-import {
-	LoginMutation,
-	LogoutMutation,
-	MeDocument,
-	MeQuery,
-	RegisterMutation,
-} from '../generated/graphql';
-
-function betterUpdateQuery<Result, Query>(
-	cache: Cache,
-	qi: QueryInput,
-	result: any,
-	fn: (r: Result, q: Query) => Query
-) {
-	return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
-}
-
-const client = createClient({
-	url: 'http://localhost:4000/graphql',
-	exchanges: [
-		dedupExchange,
-		cacheExchange({
-			updates: {
-				Mutation: {
-					login: (result: LoginMutation, _args, cache, _info) => {
-						betterUpdateQuery<LoginMutation, MeQuery>(
-							cache,
-							{ query: MeDocument },
-							result,
-							(result, query) => {
-								if (result.login.errors) {
-									return query;
-								} else {
-									return {
-										me: result.login.user,
-									};
-								}
-							}
-						);
-					},
-					register: (
-						result: RegisterMutation,
-						_args,
-						cache,
-						_info
-					) => {
-						betterUpdateQuery<RegisterMutation, MeQuery>(
-							cache,
-							{ query: MeDocument },
-							result,
-							(result, query) => {
-								if (result.register.errors) {
-									return query;
-								} else {
-									return {
-										me: result.register.user,
-									};
-								}
-							}
-						);
-					},
-					logout: (_result, _args, cache, _info) => {
-						betterUpdateQuery<LogoutMutation, MeQuery>(
-							cache,
-							{ query: MeDocument },
-							_result,
-							() => ({ me: null })
-						);
-					},
-				},
-			},
-		}),
-		fetchExchange,
-	],
-	fetchOptions: {
-		credentials: 'include',
-	},
-});
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<Provider value={client}>
-			<ThemeProvider theme={theme}>
-				<CSSReset />
-				<Component {...pageProps} />
-			</ThemeProvider>
-		</Provider>
+		<ThemeProvider theme={theme}>
+			<CSSReset />
+			<Component {...pageProps} />
+		</ThemeProvider>
 	);
 }
 
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { withUrqlClient } from 'next-urql';
 import { NavBar } from '../components/NavBar';
+import { createUrqlClient } from '../utils/createUrqlClient';
 
 const Index = () => {
 	return (
@@ -10,4 +12,4 @@ const Index = () => {
 	);
 };
 
-export default Index;
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
