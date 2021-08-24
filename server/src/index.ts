9a70652e2479a9e570e140f6895b208d6575faf9
Dan Bennett <dghbennett47@gmail.com>
2021-08-24 17:24:34 +0100

    feat: replace mikro orm with typeorm

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,5 @@
 import 'reflect-metadata';
-import { MikroORM } from '@mikro-orm/core';
 import { USER_COOKIE, __prod__ } from './constants';
-import microConfig from './mikro-orm.config';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
@@ -12,11 +10,20 @@ import Redis from 'ioredis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import cors from 'cors';
+import { createConnection } from 'typeorm';
+import { Post } from './entities/Post';
+import { User } from './entities/User';
 
 const main = async () => {
-	const orm = await MikroORM.init(microConfig);
-
-	await orm.getMigrator().up();
+	const conn = await createConnection({
+		type: 'postgres',
+		database: 'lireddit',
+		username: 'postgres',
+		password: 'postgres',
+		logging: true,
+		synchronize: true,
+		entities: [Post, User],
+	});
 
 	const app = express();
 
@@ -58,7 +65,7 @@ const main = async () => {
 			resolvers: [HelloResolver, PostResolver, UserResolver],
 			validate: false,
 		}),
-		context: ({ req, res }) => ({ em: orm.em, req, res, redis }),
+		context: ({ req, res }) => ({ req, res, redis }),
 	});
 
 	apolloServer.applyMiddleware({
--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -10,12 +10,12 @@ import {
 } from 'type-graphql';
 import { User } from '../entities/User';
 import argon2 from 'argon2';
-import { EntityManager } from '@mikro-orm/postgresql';
 import { FORGET_PASSWORD_PREFIX, USER_COOKIE } from '../constants';
 import { UsernamePasswordInput } from './UsernamePasswordInput';
 import { validateRegister } from '../utils/validateRegister';
 import { sendEmail } from '../utils/sendEmail';
 import { v4 } from 'uuid';
+import { getConnection } from 'typeorm';
 
 @ObjectType()
 class FieldError {
@@ -40,7 +40,7 @@ export class UserResolver {
 	async changePassword(
 		@Arg('token') token: string,
 		@Arg('newPassword') newPassword: string,
-		@Ctx() { em, redis, req }: MyContext
+		@Ctx() { redis, req }: MyContext
 	): Promise<UserResponse> {
 		if (newPassword.length <= 4) {
 			return {
@@ -68,7 +68,8 @@ export class UserResolver {
 			};
 		}
 
-		const user = await em.findOne(User, { id: parseInt(userId) });
+		const userIdNum = parseInt(userId);
+		const user = await User.findOne(userIdNum);
 
 		if (!user) {
 			return {
@@ -81,8 +82,10 @@ export class UserResolver {
 			};
 		}
 
-		user.password = await argon2.hash(newPassword);
-		await em.persistAndFlush(user);
+		User.update(
+			{ id: userIdNum },
+			{ password: await argon2.hash(newPassword) }
+		);
 
 		await redis.del(key);
 
@@ -94,13 +97,12 @@ export class UserResolver {
 	@Mutation(() => Boolean)
 	async forgotPassword(
 		@Arg('usernameOrEmail') usernameOrEmail: string,
-		@Ctx() { em, redis }: MyContext
+		@Ctx() { redis }: MyContext
 	) {
-		const user = await em.findOne(
-			User,
+		const user = await User.findOne(
 			usernameOrEmail.includes('@')
-				? { email: usernameOrEmail }
-				: { username: usernameOrEmail }
+				? { where: { email: usernameOrEmail } }
+				: { where: { username: usernameOrEmail } }
 		);
 		if (!user) {
 			// not in db
@@ -127,20 +129,19 @@ export class UserResolver {
 	}
 
 	@Query(() => User, { nullable: true })
-	async me(@Ctx() { em, req }: MyContext) {
+	me(@Ctx() { req }: MyContext) {
 		if (!req.session.userId) {
 			// not logged in
 			return null;
 		}
 
-		const user = await em.findOne(User, { id: req.session.userId });
-		return user;
+		return User.findOne(req.session.userId);
 	}
 
 	@Mutation(() => UserResponse)
 	async register(
 		@Arg('options') options: UsernamePasswordInput,
-		@Ctx() { em, req }: MyContext
+		@Ctx() { req }: MyContext
 	): Promise<UserResponse> {
 		const errors = validateRegister(options);
 		if (errors) {
@@ -151,18 +152,18 @@ export class UserResolver {
 
 		let user;
 		try {
-			const response = await (em as EntityManager)
-				.createQueryBuilder(User)
-				.getKnexQuery()
-				.insert({
+			const result = await getConnection()
+				.createQueryBuilder()
+				.insert()
+				.into(User)
+				.values({
 					username: options.username,
 					password: hashedPassword,
 					email: options.email,
-					created_at: new Date(), // _ instead of camelCase as mikroOrm changes automatically to underscores
-					updated_at: new Date(),
 				})
-				.returning('*');
-			user = response[0];
+				.returning('*')
+				.execute();
+			user = result.raw[0];
 		} catch (err) {
 			if (err.code === '23505') {
 				if (err.detail.includes('email')) {
@@ -190,6 +191,7 @@ export class UserResolver {
 		}
 
 		// set login cookie
+		console.log('user.id: ', user.id);
 		req.session.userId = user.id;
 
 		return { user };
@@ -199,14 +201,14 @@ export class UserResolver {
 	async login(
 		@Arg('usernameOrEmail') usernameOrEmail: string,
 		@Arg('password') password: string,
-		@Ctx() { em, req }: MyContext
+		@Ctx() { req }: MyContext
 	): Promise<UserResponse> {
-		const user = await em.findOne(
-			User,
+		const user = await User.findOne(
 			usernameOrEmail.includes('@')
-				? { email: usernameOrEmail }
-				: { username: usernameOrEmail }
+				? { where: { email: usernameOrEmail } }
+				: { where: { username: usernameOrEmail } }
 		);
+
 		if (!user) {
 			return {
 				errors: [
@@ -241,7 +243,6 @@ export class UserResolver {
 			req.session.destroy((err) => {
 				res.clearCookie(USER_COOKIE);
 				if (err) {
-					console.log('err: ', err);
 					resolve(false);
 					return;
 				}
