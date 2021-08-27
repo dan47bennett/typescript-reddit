95696af24244fad04b1e0eb85d41f60aad5a056e
Dan Bennett <dghbennett47@gmail.com>
2021-08-27 23:35:50 +0100

    feat: add create post page

---
+++ web/src/pages/create-post.tsx
@@ -0,0 +1,61 @@
+import { Box, Button } from '@chakra-ui/core';
+import { Form, Formik } from 'formik';
+import { withUrqlClient } from 'next-urql';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { InputField } from '../components/InputField';
+import { Layout } from '../components/Layout';
+import { useCreatePostMutation } from '../generated/graphql';
+import { createUrqlClient } from '../utils/createUrqlClient';
+
+const CreatePost: React.FC<{}> = ({}) => {
+	const router = useRouter();
+	const [, createPost] = useCreatePostMutation();
+	return (
+		<Layout variant="small">
+			<Formik
+				initialValues={{ title: '', text: '' }}
+				onSubmit={async (values) => {
+					const error = await createPost({ input: values });
+					if (!error) {
+						router.push('/');
+					}
+				}}
+			>
+				{({ isSubmitting }) => (
+					<Form>
+						<InputField
+							name="title"
+							placeholder="title"
+							label="Title"
+						/>
+						<Box mt={4}>
+							<InputField
+								textArea={true}
+								name="text"
+								placeholder="Write your post here"
+								label="Your post"
+							/>
+						</Box>
+						<Box
+							display="flex"
+							flexDirection="row"
+							alignItems="center"
+							mt={4}
+						>
+							<Button
+								type="submit"
+								isLoading={isSubmitting}
+								variantColor="teal"
+							>
+								Post
+							</Button>
+						</Box>
+					</Form>
+				)}
+			</Formik>
+		</Layout>
+	);
+};
+
+export default withUrqlClient(createUrqlClient)(CreatePost);
